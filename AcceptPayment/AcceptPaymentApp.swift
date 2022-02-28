0477f859d29aaa11dc5f370fe1b7751203231de7
Terrel <TerrelRJones@gmail.com>
2022-02-28 15:41:11 -0800

    Initial Commit

---
+++ AcceptPayment/AcceptPaymentApp.swift
@@ -0,0 +1,17 @@
+//
+//  AcceptPaymentApp.swift
+//  AcceptPayment
+//
+//  Created by Terrel Jones on 2/28/22.
+//
+
+import SwiftUI
+
+@main
+struct AcceptPaymentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AcceptPayment/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AcceptPayment
+//
+//  Created by Terrel Jones on 2/28/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
